f5765b210c163ade828b5e5d3d5fa067d00aa668
adheenapk <adheenapkckl@gmail.com>
2023-06-28 11:20:14 +0530

    first-commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,116 @@
-import React from 'react'
-import './home.css'
+
 import spotify from '../assets/spotify.png'
 import spot from '../assets/spot.png'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
-const Home = () => {
+const Navigation=styled.nav `position: absolute;
+height: 5rem ;
+width: 100%;
+background: black;
+opacity: 70%;
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+
+.image img {
+    width: 10rem;
+    height: 3rem;
+    margin-left: 15rem;
+  }
+  
+  li {
+    list-style: none;
+    display: inline-block;
+  }
+  
+  li {
+    color: white;
+    font-size: 1.5rem;
+    margin-right: 3rem;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+`;
+const Container=styled.div `display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+
+.btn{
+    color: white;
+    position: absolute;
+    top: 34rem;
+    left: 49rem;
+    background-color: #03C04A;
+    border-radius: 10rem;
+    width: 9rem;
+    height: 1.5rem;
+    font-size: 1rem;
+    font-weight: bold;
+    border: #03C04A;
+}
+
+.bg img{
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+    height: 100vh;
+    width: 100%;
+  
+}
+.music{
+    position: absolute;
+    top: 20rem;
+    left: 27rem;
+    color: whitesmoke;
+    font-size: 6rem;
+    font-weight:1000;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+.mill{
+    position: absolute;
+    top: 28rem;
+    left: 40rem;
+    color: whitesmoke;
+    font-size: 1.8rem;
+    font-weight:900;
+    font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+}
+.count{
+  position: absolute;
+    top: 30rem;
+    left: 50rem;
+    color: whitesmoke;
+    font-size: 1.5rem;
+    font-weight:900;
+}
+#v1{
+  color: orangered;
+  width: 1rem;
+  border-radius: 10rem;
+  text-align: center;
+  background-color: #f8f8f6;
+  border: yellow;
+}
+@media screen and (max-width:400px){
+  .music{
+    position: absolute;
+    color: whitesmoke;
+    font-size: 13px;
+    font-weight:1000;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  
+}
+}
+`;
+
+function Home() {
+  const[value,setValue]=useState(0)
+
+ useEffect(()=>{document.getElementById('v1').value=value},[value])
   return (
     <>
-      <nav className="navbar">
+      <Navigation>
         <div className="image">
           <img src={spot}></img>
         </div>
@@ -20,8 +124,8 @@ const Home = () => {
             <li>Log In</li>
           </ul>
         </div>
-      </nav>
-      <div className='main'>
+      </Navigation>
+      <Container>
         <div className='bg'>
           <img src={spotify}></img>
           <div className='music'>
@@ -29,10 +133,12 @@ const Home = () => {
         </div>
         <div className="mill">Millions of songs.No credit card needed.
         </div>
-        <div className="sub"><input type="submit" value="UPDATE"></input></div>
+        <h1 className='count'>Count : <input onChange={(e)=>console.log(e.target.value)} type="text"  id='v1'/></h1>
+        <button onClick={()=>setValue(value+1)} className="btn">Update</button>
+     
         </div>
        
-      </div>
+      </Container>
 
     </>
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,115 @@
-import React from 'react'
 
-const Login = () => {
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const Container=styled.div ` background-color: #008ECC;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+height: 930px;
+.log{
+    background-color: whitesmoke;
+    height: 600px;
+    width: 500px;
+    border-radius: 10px 10px 10px 10px;
+  
+}
+.heading{
+    margin-top: 80px;
+    color: black;
+    text-align: center;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 30px;
+}
+.details{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    
+  }
+  label{
+    margin-left:30px;
+    margin-bottom: 5px;
+    margin-top: 10px;
+  }
+  input{
+    margin-left: 30px;
+    width: 450px;
+    height: 55px;
+    border: grey;
+  }
+  .check{
+    position: absolute;
+    left: 470px;
+    transform: scale(0.4);
+  }
+  .check label{
+    font-size: 40px;
+    color: rgb(48, 47, 47);
+    position: absolute;
+    font-family: serif;
+    left: 280px;
+    width: 500px;
+}
+.btn input{
+    margin-top: 80px;
+    color: white;
+    background-color: #008ECC;
+    font-size: large;
+}   
+.forg{
+    text-align: center;
+    margin-top: 30px;
+    font-size: medium;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+
+a{
+    text-decoration: none;
+    color: #008ECC;
+}
+@media all{
+    
+}
+`;
+
+
+function Login() {
+  const[value,setValue]=useState(null)
+
+ useEffect(()=>{document.getElementById('v1').value=value},[value])
+ useEffect(()=>{document.getElementById('v2').value=value},[value])
   return (
-    <div>login</div>
+    <>
+    
+    <Container>
+                <div className="log">
+                    <div className="heading">
+                        Login
+                    </div>
+                    <div className="details">
+                        <label htmlFor="mail">E-mail</label>
+                        <input onChange={(e)=>console.log(e.target.value)} type="email" id="v1" className="id" placeholder="      Enter email" required></input>
+                        <label htmlFor="pass">Password </label>
+                        <input onChange={(e)=>console.log(e.target.value)} type="password" id="v2" className="id" placeholder="       Enter password" required></input>
+                    </div>
+                    <div className="check">
+                        <input type="checkbox" id="checkbox"></input>
+                        <label htmlFor="checkbox">Show Password</label>
+                    </div>
+                    <div className="btn">
+                        <input type="submit" value="SIGN IN"></input>
+                    </div>
+                    <div className="forg">
+                        Forgot <a href="#">Username</a> / <a href="#"> Password</a> ?
+                        <div className="dont">Don't have an account? <a href="#"> Sign up</a> </div>
+                    </div>
+                </div>
+            </Container>
+
+    </>
+
   )
 }
 
